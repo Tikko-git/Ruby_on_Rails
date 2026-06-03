4a0f3f690cb44b37f3712823a41733fab7fd8b23
Tikko-git <159120967+Tikko-git@users.noreply.github.com>
2026-06-03 16:28:04 +0300

    Delete lab5/app directory

--- lab5/app/helpers/application_helper.rb
+++
@@ -1,8 +0,0 @@
-module ApplicationHelper
-  def amount_badge(amount)
-    amount ||= 0
-    badge_class = amount > 1000 ? 'bg-danger' : 'bg-success'
-
-    content_tag(:span, "#{amount} грн", class: "badge #{badge_class} shadow-sm")
-  end
-end
